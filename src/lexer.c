66638e66810671fa7c392b54d91ea62c8ab39488
JDN89 <jan.de.niels@gmail.com>
2024-08-26 21:11:01 +0200

    fix test - pass lexer as pointer

--- src/lexer.c
+++ src/lexer.c
@@ -2,30 +2,24 @@
 #include <stdbool.h>
 #include <string.h>
 
-// BUG: globally inialized lexer resets the lexer I use in tests. Remove global
-// lexer and pass pointer to lexer in all the functions. This was also a point
-// in crafting interpreters. He used a global variation to safe space. You can
-// only put so many characters on a page, but this is not a book...
-Lexer lexer;
-
 Lexer *init_lexer(char *source, Lexer *lexer) {
   lexer->start = source;
   lexer->current = source;
   return lexer;
 };
 
-static bool isAtEnd() { return *lexer.current == '\0'; }
+static bool isAtEnd(Lexer *lexer) { return *lexer->current == '\0'; }
 
-static char advance() {
-  lexer.current++;
-  return lexer.current[-1];
+static char advance(Lexer *lexer) {
+  lexer->current++;
+  return lexer->current[-1];
 }
 
-static Token makeToken(TokenType type) {
+static Token makeToken(TokenType type, Lexer *lexer) {
   Token token;
   token.type = type;
-  token.length = lexer.current - lexer.start;
-  token.literal = lexer.start;
+  token.length = lexer->current - lexer->start;
+  token.literal = lexer->start;
   return token;
 }
 static Token makeErrorToken(const char *message) {
@@ -38,13 +32,13 @@ static Token makeErrorToken(const char *message) {
 
 Token nextToken(Lexer *lexer) {
   lexer->current = lexer->start;
-  if (isAtEnd())
-    return makeToken(EOF);
+  if (isAtEnd(lexer))
+    return makeToken(EOF, lexer);
 
-  char c = advance();
+  char c = advance(lexer);
   switch (c) {
   case '=':
-    return makeToken(ASSIGN);
+    return makeToken(ASSIGN, lexer);
   }
   return makeErrorToken("Illegal string in source");
 }
--- tests/lexer_test.c
+++ tests/lexer_test.c
@@ -2,15 +2,14 @@
 #include <assert.h>
 #include <stdio.h>
 
-/*void test_init_lexer() {*/
-/*  char source[] = "=/0";*/
-/*  Lexer lexer;*/
-/*  init_lexer(source, &lexer);*/
-/**/
-/*  printf("%c", *lexer.start);*/
-/*  assert(*lexer.start == *source);*/
-/*  assert(*lexer.current == *source);*/
-/*}*/
+void test_init_lexer() {
+  char source[] = "=/0";
+  Lexer lexer;
+  init_lexer(source, &lexer);
+
+  assert(*lexer.start == *source);
+  assert(*lexer.current == *source);
+}
 
 void test_nextToken() {
   char source[] = "=/0";
@@ -18,14 +17,13 @@ void test_nextToken() {
   init_lexer(source, &lexer);
 
   Token token = nextToken(&lexer);
-  /*assert(token.type == ASSIGN);*/
-  /*printf("%c", *token.literal);*/
-  /*assert(*token.literal == '8');*/
-  /*assert(token.length == 1); // Assuming 'x' is a single character*/
+  assert(token.type == ASSIGN);
+  assert(*token.literal == '=');
+  assert(token.length == 1); // Assuming 'x' is a single character
 }
 
 int main() {
-  /*test_init_lexer();*/
+  test_init_lexer();
   test_nextToken();
   printf("All tests passed.\n");
   return 0;
