78887f55c79eb1da4bf90c887f6d97845f79d3d9
JDN89 <jan.de.niels@gmail.com>
2024-09-04 22:35:26 +0200

    init repl - but tired finish tomorrow and print output, look for test_lexer for inspiration

--- src/main.c
+++ src/main.c
@@ -1,3 +1,34 @@
 #include <stdio.h>
 
-int main() { printf("yolo"); }
+#include "lexer.h"
+#include <pwd.h>
+#include <stdio.h>
+
+static void repl(Lexer *lexer) {
+  char line[1024];
+  for (;;) {
+    printf(">> ");
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+  }
+  init_lexer(line, lexer);
+  Token token = nextToken(lexer);
+  while (token.type != TOKEN_EOF) {
+    printf("Token = %s", token.type);
+  }
+}
+
+int main(int argc, const char *argv[]) {
+
+  // TODO :read source input file
+  (void)argv;
+  printf("Hello!  This is the Monkey programming language!\n");
+  printf("Feel free to type in commands\n");
+  Lexer lexer;
+  if (argc == 1) {
+    repl(&lexer);
+  }
+  return 0;
+}
