339bd5200e1d39217b27260e34e70cca9165ded6
JDN89 <jan.de.niels@gmail.com>
2024-12-21 12:55:53 +0100

    describe bug in readme todo

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -391,8 +391,11 @@ void test_parse_if_statement() {
   assert(strcmp(if_e->condition->as.infix->left->as.identifier->value->as
                     .string->pointer,
                 identifiers[0]) == 0);
-  assert(strcmp(if_e->consequence->statements->as.letStmt->name->value->as
-                    .string->pointer,
+  printf(" consequence -- %s \n",
+         if_e->consequence->statements[if_e->consequence->count]
+             .as.letStmt->name->value->as.string->pointer);
+  assert(strcmp(if_e->consequence->statements[if_e->consequence->count]
+                    .as.letStmt->name->value->as.string->pointer,
                 identifiers[1]) == 0);
 
   printf("Parse if statement - PASSED! \n");
