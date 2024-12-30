46bbcefa91741c6ad2b11500f0ccdd11df758977
JDN89 <jan.de.niels@gmail.com>
2024-12-30 14:44:11 +0100

    debug parse function call expression

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -438,17 +438,17 @@ void test_parser_function_call(void) {
 }
 
 int main(void) {
-  test_parser_error_during_parse_let_statement();
-  test_parse_let_statement();
-  test_parse_integer_literal();
-  test_parse_return_statement();
-  test_parse_expressions();
-  test_parse_infix_expressions();
-  test_parse_values();
-  parse_prefix_expressions();
-  test_parse_if_statement();
-  test_parse_function_literal_expression();
-  /*test_parser_function_call();*/
+  /*test_parser_error_during_parse_let_statement();*/
+  /*test_parse_let_statement();*/
+  /*test_parse_integer_literal();*/
+  /*test_parse_return_statement();*/
+  /*test_parse_expressions();*/
+  /*test_parse_infix_expressions();*/
+  /*test_parse_values();*/
+  /*parse_prefix_expressions();*/
+  /*test_parse_if_statement();*/
+  /*test_parse_function_literal_expression();*/
+  test_parser_function_call();
   printf("\n");
   return 0;
 }
